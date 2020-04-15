f49901a40270865dca68cd24d63d363298b97004
Gleryston Matos <glerystonmatos@yahoo.com.br>
2020-04-15 07:31:44 -0300

    Criação dos exercícios da prova de logica de programação.

---
+++ Prova Logica de Programação/ExerciciosLogica/Peca.cs
@@ -0,0 +1,11 @@
+﻿namespace ExerciciosLogica
+{
+    public class Peca
+    {
+        public int Codigo { get; set; }
+
+        public int Numero { get; set; }
+
+        public double Valor { get; set; }
+    }
+}
