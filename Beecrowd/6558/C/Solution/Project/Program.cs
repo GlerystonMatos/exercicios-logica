9bff2c7e82fc087c8e7d02d9bd2e71b1fd99d662
Gleryston Matos <glerystonmatos@yahoo.com.br>
2023-03-25 15:23:29 -0300

    Beecrowd torneio 6558

---
+++ Beecrowd/6558/C/Solution/Project/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        IList<string> entradas = new List<string>();
+        while (true)
+        {
+            string frase = Console.ReadLine();
+            if (!frase.Equals("*"))
+            {
+                entradas.Add(frase);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        IList<string> saidas = new List<string>();
+        foreach (string frase in entradas)
+        {
+            char inicial = ' ';
+            string tautograma = "Y";
+            string[] palavras = frase.Split(' ');
+
+            foreach (string palavra in palavras)
+            {
+                char[] letras = palavra.ToUpper().ToCharArray();
+                if (inicial.Equals(' '))
+                    inicial = letras[0];
+
+                if (!inicial.Equals(letras[0]))
+                {
+                    tautograma = "N";
+                    break;
+                }
+            }
+
+            saidas.Add(tautograma);
+        }
+
+        foreach (string tautograma in saidas)
+        {
+            Console.WriteLine("{0}", tautograma);
+        }
+    }
+}
