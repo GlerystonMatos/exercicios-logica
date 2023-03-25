641215a06a25e8a97104df732500ddfe1d51ba5b
Gleryston Matos <glerystonmatos@yahoo.com.br>
2023-03-25 17:15:29 -0300

    Beecrowd torneio 6628

---
+++ Beecrowd/6628/A/Solution/Project/Program.cs
@@ -0,0 +1,37 @@
+﻿class URI
+{
+    static void Main(string[] args)
+    {
+        int quantidade = 0;
+        IList<int> quantidadeDiamantes = new List<int>();
+        int.TryParse(Console.ReadLine(), out quantidade);
+
+        for (int casoTeste = 0; casoTeste < quantidade; casoTeste++)
+        {
+            string valor = Console.ReadLine();
+            char[] valores = valor.Trim().ToCharArray();
+
+            int diamantes = 0;
+            int fechamento = 0;
+            int inicioDiamante = 0;
+
+            for (int contador = 0; contador < valores.Length; contador++)
+            {
+                if (valores[contador] == '<')
+                    inicioDiamante += 1;
+                else if (valores[contador] == '>' && inicioDiamante > 0)
+                {
+                    inicioDiamante -= 1;
+                    diamantes += 1;
+                }
+            }
+
+            quantidadeDiamantes.Add(diamantes);
+        }
+
+        foreach (int diamantes in quantidadeDiamantes)
+        {
+            Console.Write("{0}\n", diamantes);
+        }
+    }
+}
---
+++ Beecrowd/6628/C/Solution/Project/Program.cs
@@ -0,0 +1,23 @@
+﻿class URI
+{
+    static void Main(string[] args)
+    {
+        string entrada = Console.ReadLine();
+        string[] valores = entrada.Trim().Split(' ');
+
+        int p1 = int.Parse(valores[0]);
+        int c1 = int.Parse(valores[1]);
+        int p2 = int.Parse(valores[2]);
+        int c2 = int.Parse(valores[3]);
+
+        int esquerda = p1 * c1;
+        int direita = p2 * c2;
+
+        if (esquerda == direita)
+            Console.Write("{0}\n", 0);
+        else if (esquerda > direita)
+            Console.Write("{0}\n", -1);
+        else
+            Console.Write("{0}\n", 1);
+    }
+}
